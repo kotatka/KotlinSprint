51c35237a9d280c65aa40f51b9ab9a68c02afd65
kotatka <tyrrrr5665@gmail.com>
2024-09-26 16:37:54 +0300

    KS-2-5 do smth

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -1,10 +1,11 @@
 package lesson_2
 
 
-fun main(){
+fun main() {
+    val percentConst = 100
     val sumDeposit = 70000
     val interestRate = 16.7
     val durationOfDeposite = 20
-    val finalSum:Double = sumDeposit*(1+(interestRate/100))*durationOfDeposite
+    val finalSum: Double = sumDeposit * (1 + (interestRate / percentConst)) * durationOfDeposite
     println("%.3f".format(finalSum))
 }
