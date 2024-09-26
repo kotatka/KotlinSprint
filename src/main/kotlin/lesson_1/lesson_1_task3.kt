eab748ab54677f5101b5db00eeba262405ac0851
kotatka <tyrrrr5665@gmail.com>
2024-09-26 16:00:04 +0300

    KS-1-3 do smth

--- src/main/kotlin/lesson_1/lesson_1_task3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task3.kt
@@ -1,14 +1,14 @@
 package lesson_1
 
-fun main(){
+fun main() {
     val year: Short = 1961
-    var hour: Byte = 9
-    var minute: Byte = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println(year)
     println(hour)
     println(minute)
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print(hour)
     print(":")
     print(minute)
