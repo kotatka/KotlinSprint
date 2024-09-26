15418e9cab0621402ff05175af36032b1c5a801f
kotatka <tyrrrr5665@gmail.com>
2024-09-26 15:18:43 +0300

    do task 4

---
+++ src/main/kotlin/lesson_3/lesson_3_task4.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main(){
+    var startMove = "E2"
+    var endMove = "E4"
+    var numberTurn = 1
+    println("$startMove - $endMove; ход - $numberTurn")
+    startMove = "D2"
+    endMove = "D3"
+    numberTurn++
+    println("$startMove - $endMove; ход - $numberTurn")
+}
