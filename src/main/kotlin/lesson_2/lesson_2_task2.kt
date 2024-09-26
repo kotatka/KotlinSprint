855658aef5028a7259ba613a383bc970b9279afd
kotatka <tyrrrr5665@gmail.com>
2024-09-26 16:22:37 +0300

    KS-2-2 do smth

--- src/main/kotlin/lesson_2/lesson_2_task2.kt
+++ src/main/kotlin/lesson_2/lesson_2_task2.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
-fun main(){
+fun main() {
     val salWorker = 30000
     val salIntern = 20000
     val sumWorker = 50
     val sumIntern = 30
-    val costWorker = salWorker*sumWorker
-    val costAll = costWorker+salIntern*sumIntern
-    val costAverage = costAll/(sumIntern+sumWorker)
+    val costWorker = salWorker * sumWorker
+    val costAll = costWorker + salIntern * sumIntern
+    val costAverage = costAll / (sumIntern + sumWorker)
     println(costWorker)
     println(costAll)
     println(costAverage)
