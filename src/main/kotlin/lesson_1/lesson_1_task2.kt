b5062ee1d359c4fe262010801a85e859be321973
kotatka <tyrrrr5665@gmail.com>
2024-09-26 08:30:42 +0300

    do task 2

---
+++ src/main/kotlin/lesson_1/lesson_1_task2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main () {
+    val numOfOrders: Byte = 75
+    val textThank: String = "Спасибо за покупку!Ждём вас снова!"
+    println(numOfOrders)
+    println(textThank)
+    var numWorkers: Short = 2000
+    println(numWorkers)
+    numWorkers--
+    println(numWorkers)
+
+}
