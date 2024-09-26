62ccf837fa1ee5c385f0b4f2bc81c46c56067732
kotatka <tyrrrr5665@gmail.com>
2024-09-26 14:43:29 +0300

    do task 1

---
+++ src/main/kotlin/lesson_3/lesson_3_task1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main(){
+    var welcomeStr = "Добрый день"
+    val userName = "Даниил"
+    println("$welcomeStr, $userName!")
+    welcomeStr = "Добрый вечер"
+    println("$welcomeStr, $userName!")
+}
