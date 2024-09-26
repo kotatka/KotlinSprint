3ea04549e4000a4dbe4ae64b87fc609727fc9aad
kotatka <tyrrrr5665@gmail.com>
2024-09-26 10:41:36 +0300

    do task 1

---
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -0,0 +1,9 @@
+package lesson_2
+
+fun main(){
+    val firstStudent:Float = 3f
+    val secondStudent:Float = 4f
+    val thirdStudent:Float = 3f
+    val fourthStudent:Float = 5f
+    println((firstStudent+secondStudent+thirdStudent+fourthStudent)/4)
+}
