66b030be054b2b86507648b6c3bb9340ec88af5b
kotatka <tyrrrr5665@gmail.com>
2024-09-26 08:21:42 +0300

    do smth

---
+++ src/main/kotlin/lesson_1/lesson_1_task1.kt
@@ -0,0 +1,8 @@
+package lesson_1
+
+fun main (){
+    val numOfOrders: Byte = 75
+    val textThank: String = "Спасибо за покупку!Ждём вас снова!"
+    println(numOfOrders)
+    println(textThank)
+}
