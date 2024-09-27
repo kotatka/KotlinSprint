ae99db000b5deee589eb7f9d61e7321f63aa556e
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:15:20 +0300

    KS-4-3

--- src/main/kotlin/lesson_4/lesson_4_task3_favorable_conditions.kt
+++ src/main/kotlin/lesson_4/lesson_4_task3_favorable_conditions.kt
@@ -4,10 +4,12 @@ const val SUN_WEATHER: Boolean = true
 const val OPEN_AWNING: Boolean = true
 const val AIR_HUMIDITY: Int = 20
 const val BAD_SEASON: String = "winter"
+
 fun main() {
     val weatherToday: Boolean = true
     val openAwningToday: Boolean = true
     val airHumidityToday: Int = 20
     val seasonToday: String = "winter"
+
     println("Благоприятные ли условия сейчас для роста бобовых? ${(SUN_WEATHER == weatherToday) && (OPEN_AWNING == openAwningToday) && (AIR_HUMIDITY == airHumidityToday) && (BAD_SEASON != seasonToday)}")
 }
