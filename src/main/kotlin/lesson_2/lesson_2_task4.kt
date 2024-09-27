f525298e2d452e0b76bd4c40246f1d3d1f56b400
kotatka <tyrrrr5665@gmail.com>
2024-09-27 10:08:43 +0300

    KS-2-54-resourse-buff

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -1,12 +1,11 @@
 package lesson_2
-
+const val Percent_Const = 100
 fun main() {
-    val percentCons = 100
     val crystalOre = 7
     val ironOre = 11
     val buff = 20f
-    val buffCrystal: Int = (crystalOre * buff / percentCons).toInt()
-    val buffIron: Int = (ironOre * buff / percentCons).toInt()
+    val buffCrystal: Int = (crystalOre * buff / Percent_Const).toInt()
+    val buffIron: Int = (ironOre * buff / Percent_Const).toInt()
     println(buffCrystal)
     println(buffIron)
 }
