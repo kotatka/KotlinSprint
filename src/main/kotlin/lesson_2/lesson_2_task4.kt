9536a54cb4683e1580d868e449006e87e6d3955b
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:10:55 +0300

    KS-2-4

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -1,11 +1,14 @@
 package lesson_2
+
 const val PERCENT_CONST = 100
+
 fun main() {
     val crystalOre = 7
     val ironOre = 11
     val buff = 20f
     val buffCrystal: Int = (crystalOre * buff / PERCENT_CONST).toInt()
     val buffIron: Int = (ironOre * buff / PERCENT_CONST).toInt()
+
     println(buffCrystal)
     println(buffIron)
 }
