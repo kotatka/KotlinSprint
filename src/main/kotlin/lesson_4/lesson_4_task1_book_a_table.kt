5ea770381df3b8613a196bda790b8b44ff810bab
kotatka <tyrrrr5665@gmail.com>
2024-09-27 10:55:25 +0300

    KS-4-1-book-a-table

---
+++ src/main/kotlin/lesson_4/lesson_4_task1_book_a_table.kt
@@ -0,0 +1,12 @@
+package lesson_4
+
+fun main() {
+    val allTables = 13
+    val bookTablesToday = 13
+    val freeTablesTomorrow = 4
+    val bookTablesTomorrow = allTables - freeTablesTomorrow
+    var canBook = bookTablesToday < allTables
+    println("Доступность столиков на сегодня:$canBook")
+    canBook = bookTablesTomorrow < allTables
+    println("Доступность столиков на завтра:$canBook")
+}
