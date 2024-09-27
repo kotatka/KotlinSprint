3542ac6b2b7123e77ee666cfcf1a253e490c277d
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:13:49 +0300

    KS-3-4

--- src/main/kotlin/lesson_3/lesson_3_task4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task4.kt
@@ -4,9 +4,12 @@ fun main() {
     var startMove = "E2"
     var endMove = "E4"
     var numberTurn = 1
+
     println("$startMove-$endMove;$numberTurn")
+
     startMove = "D2"
     endMove = "D3"
     numberTurn++
+
     println("$startMove-$endMove;$numberTurn")
 }
