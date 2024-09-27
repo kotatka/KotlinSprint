79ce820857d59ce5f0d74a641aed1ff91ddf082c
kotatka <tyrrrr5665@gmail.com>
2024-09-27 10:20:41 +0300

    KS-2-4-calculate-buff

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -1,11 +1,11 @@
 package lesson_2
-const val Percent_Const = 100
+const val PERCENT_CONST = 100
 fun main() {
     val crystalOre = 7
     val ironOre = 11
     val buff = 20f
-    val buffCrystal: Int = (crystalOre * buff / Percent_Const).toInt()
-    val buffIron: Int = (ironOre * buff / Percent_Const).toInt()
+    val buffCrystal: Int = (crystalOre * buff / PERCENT_CONST).toInt()
+    val buffIron: Int = (ironOre * buff / PERCENT_CONST).toInt()
     println(buffCrystal)
     println(buffIron)
 }
