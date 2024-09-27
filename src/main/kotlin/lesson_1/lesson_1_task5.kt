354619eaa07593e249a2a6ab8ddb7a3e1be0e453
kotatka <tyrrrr5665@gmail.com>
2024-09-27 10:05:52 +0300

    KS-1-5-time-in-space

--- src/main/kotlin/lesson_1/lesson_1_task5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task5.kt
@@ -1,12 +1,11 @@
 package lesson_1
-
+const val For_Time = 60
 fun main() {
-    val constForTime = 60
     val seconds = 6480
-    val minutes = seconds / constForTime
-    val hour = (minutes / constForTime)
-    val remSeconds = seconds % constForTime
-    val remMinutes = minutes % constForTime
+    val minutes = seconds / For_Time
+    val hour = (minutes / For_Time)
+    val remSeconds = seconds % For_Time
+    val remMinutes = minutes % For_Time
     println("%02d:%02d:%02d".format(hour, remMinutes, remSeconds))
 
 }
