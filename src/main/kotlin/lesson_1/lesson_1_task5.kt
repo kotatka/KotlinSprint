5ed7f702a07a4e5f0fd0f98dd0063ccedaedb913
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:09:09 +0300

    KS-1-5

--- src/main/kotlin/lesson_1/lesson_1_task5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task5.kt
@@ -1,11 +1,15 @@
 package lesson_1
+
 const val FOR_TIME = 60
+
 fun main() {
     val seconds = 6480
     val minutes = seconds / FOR_TIME
     val hour = (minutes / FOR_TIME)
     val remSeconds = seconds % FOR_TIME
     val remMinutes = minutes % FOR_TIME
+
     println("%02d:%02d:%02d".format(hour, remMinutes, remSeconds))
+
 
 }
