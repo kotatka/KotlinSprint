159f7eff86f7b0f0a64cb49c803643ab22e195a2
kotatka <tyrrrr5665@gmail.com>
2024-09-27 10:10:11 +0300

    KS-2-5-calculate-deposit

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -1,11 +1,10 @@
 package lesson_2
 
-
+const val Percent_Const = 100
 fun main() {
-    val percentConst = 100
     val sumDeposit = 70000
     val interestRate = 16.7
     val durationOfDeposite = 20
-    val finalSum: Double = sumDeposit * (1 + (interestRate / percentConst)) * durationOfDeposite
+    val finalSum: Double = sumDeposit * (1 + (interestRate / Percent_Const)) * durationOfDeposite
     println("%.3f".format(finalSum))
 }
