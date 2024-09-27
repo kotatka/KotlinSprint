d924ebbd1a1db44a76a2e7d2c2fe3ad224a1fbf8
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:17:22 +0300

    KS-4-5

--- src/main/kotlin/lesson_4/lesson_4_task5_cheak_ship.kt
+++ src/main/kotlin/lesson_4/lesson_4_task5_cheak_ship.kt
@@ -9,12 +9,17 @@ const val FAVORABLE_WEATHER = true
 fun main() {
     println("Укажите, есть ли у корабля повреждения. 'true' - если есть, 'false' - если нет")
     val damageShip = readln().toBoolean()
+
     println("Укажите количество экипажа")
     val numberOfCrew = readln().toInt()
+
     println("Укажите количество ящиков провизии")
     val quantityOfProvisions = readln().toInt()
+
     println("Укажите благоприятность метеусловий.  'true' - если благоприятнаые, 'false' - если неблагопирятные")
     val favorableWeather = readln().toBoolean()
-    println("Готовность корабля к плаванию:${((damageShip== DAMAGE_SHIP)&&(numberOfCrew<= MAX_NUMBER_OF_CREW)&&(numberOfCrew>= MIN_NUMBER_OF_CREW)&&(quantityOfProvisions> QUANTITY_OF_PROVISIONS)&&(favorableWeather== FAVORABLE_WEATHER))||
+
+    println("Готовность корабля к плаванию:" +
+            "${((damageShip== DAMAGE_SHIP)&&(numberOfCrew<= MAX_NUMBER_OF_CREW)&&(numberOfCrew>= MIN_NUMBER_OF_CREW)&&(quantityOfProvisions> QUANTITY_OF_PROVISIONS)&&(favorableWeather== FAVORABLE_WEATHER))||
             ((numberOfCrew== MAX_NUMBER_OF_CREW)&&(favorableWeather== FAVORABLE_WEATHER)&&(quantityOfProvisions> QUANTITY_OF_PROVISIONS))}")
 }
