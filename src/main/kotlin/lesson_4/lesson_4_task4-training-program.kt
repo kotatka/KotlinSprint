f2c002af542c2568e9f820fd1181eb9acf67d9d2
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:15:38 +0300

    KS-4-4

--- src/main/kotlin/lesson_4/lesson_4_task4-training-program.kt
+++ src/main/kotlin/lesson_4/lesson_4_task4-training-program.kt
@@ -2,8 +2,10 @@ package lesson_4
 
 const val GROUP_TRAINING_ONE = 1
 const val GROUP_TRAINING_TWO = 0
+
 fun main() {
     val trainingDay = 5
+
     println(
         """
         Упражнения для рук:    ${(trainingDay % 2) == GROUP_TRAINING_ONE}
