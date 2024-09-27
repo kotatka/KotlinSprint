5d26cf0bc12032b7722ba85400829cf93c9fbcd7
kotatka <tyrrrr5665@gmail.com>
2024-09-27 15:12:52 +0300

    KS-3-2

--- src/main/kotlin/lesson_3/lesson_3_task2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task2.kt
@@ -5,8 +5,11 @@ fun main() {
     val name = "Татьяна"
     val patronymic = "Сергеевна"
     var age = 20
+
     println("$surname $name $patronymic, $age")
+
     age = 22
     surname = "Сидорова"
+
     println("$surname $name $patronymic, $age")
 }
