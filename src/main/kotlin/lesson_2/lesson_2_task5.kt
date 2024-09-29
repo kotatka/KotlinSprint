092daed5401a59faff273cf47998f93f8f06bbeb
kotatka <tyrrrr5665@gmail.com>
2024-09-29 08:52:52 +0300

    KS-5-2-age-verification

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -1,12 +1,11 @@
 package lesson_2
 
-const val PERCENT_CONST1 = 100
 
 fun main() {
     val sumDeposit = 70000
     val interestRate = 16.7
     val durationOfDeposite = 20
-    val finalSum: Double = sumDeposit * (1 + (interestRate / PERCENT_CONST1)) * durationOfDeposite
+    val finalSum: Double = sumDeposit * (1 + (interestRate / PERCENT_CONST)) * durationOfDeposite
 
     println("%.3f".format(finalSum))
 }
