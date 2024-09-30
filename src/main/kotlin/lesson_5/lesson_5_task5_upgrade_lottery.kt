3189424a5479c15b6fccd45ab16e88b38a8021f4
kotatka <tyrrrr5665@gmail.com>
2024-09-30 07:30:52 +0300

    KS-5-5-upgrade-lottery

---
+++ src/main/kotlin/lesson_5/lesson_5_task5_upgrade_lottery.kt
@@ -0,0 +1,19 @@
+package lesson_5
+
+fun main() {
+    val lotteryNums = ( (0..42).shuffled().take(3) ).toList()
+
+    println("Введите 3 числа")
+
+    val userChoise = listOf(readln().toInt(), readln().toInt(), readln().toInt())
+    val guessedNums = (lotteryNums.intersect(userChoise)).toList()
+    val sizeOfPrice = guessedNums.size
+
+    when (sizeOfPrice) {
+        3 -> println("Поздравляем, вы угадали все числа! Вы получаете Джекпот!!!")
+        2 -> println("Вы угадали два числа. Вы получаете крупный приз.")
+        1 -> println("Вы угадали одно число. Вы получаете утешительный приз.")
+        else -> println("Вы не угадали ни одного числа. Попробуйте еще раз.")
+    }
+    println("Выигрышные номера: $lotteryNums")
+}
