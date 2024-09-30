7ca6bc0b77fc2fadc0f58d165615a273fcb9b2f6
kotatka <tyrrrr5665@gmail.com>
2024-09-30 06:20:25 +0300

    KS-4-3

--- src/main/kotlin/lesson_4/lesson_4_task3_favorable_conditions.kt
+++ src/main/kotlin/lesson_4/lesson_4_task3_favorable_conditions.kt
@@ -1,16 +1,16 @@
 package lesson_4
 
-const val SUN_WEATHER: Boolean = true
-const val OPEN_AWNING: Boolean = true
+const val IS_SUN_WEATHER: Boolean = true
+const val IS_OPEN_AWNING: Boolean = true
 const val AIR_HUMIDITY: Int = 20
 const val BAD_SEASON: String = "winter"
 
 fun main() {
-    val weatherToday: Boolean = true
-    val openAwningToday: Boolean = true
+    val isSunny: Boolean = true
+    val isOpenAwningToday: Boolean = true
     val airHumidityToday: Int = 20
     val seasonToday: String = "winter"
-    val isTrueCondiditon = (SUN_WEATHER == weatherToday) && (OPEN_AWNING == openAwningToday) &&
+    val isTrueCondiditon = (IS_SUN_WEATHER == isSunny) && (IS_OPEN_AWNING == isOpenAwningToday) &&
             (AIR_HUMIDITY == airHumidityToday) && (BAD_SEASON != seasonToday)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isTrueCondiditon")
