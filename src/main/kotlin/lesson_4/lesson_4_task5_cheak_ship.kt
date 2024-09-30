14b51542f992282ade0e5cd282523bbfa9302cad
kotatka <tyrrrr5665@gmail.com>
2024-09-30 06:31:15 +0300

    KS-4-5

--- src/main/kotlin/lesson_4/lesson_4_task5_cheak_ship.kt
+++ src/main/kotlin/lesson_4/lesson_4_task5_cheak_ship.kt
@@ -1,14 +1,14 @@
 package lesson_4
 
-const val DAMAGE_SHIP = false
+const val IS_DAMAGE_SHIP = false
 const val MIN_NUMBER_OF_CREW = 55
 const val MAX_NUMBER_OF_CREW = 70
 const val QUANTITY_OF_PROVISIONS = 50
-const val FAVORABLE_WEATHER = true
+const val IS_FAVORABLE_WEATHER = true
 
 fun main() {
     println("Укажите, есть ли у корабля повреждения. 'true' - если есть, 'false' - если нет")
-    val damageShip = readln().toBoolean()
+    val isDamageShip = readln().toBoolean()
 
     println("Укажите количество экипажа")
     val numberOfCrew = readln().toInt()
@@ -17,14 +17,14 @@ fun main() {
     val quantityOfProvisions = readln().toInt()
 
     println("Укажите благоприятность метеусловий.  'true' - если благоприятнаые, 'false' - если неблагопирятные")
-    val favorableWeather = readln().toBoolean()
+    val isFavorableWeather = readln().toBoolean()
 
     println(
         "Готовность корабля к плаванию:" +
                 "${
-                    ((damageShip == DAMAGE_SHIP) && (numberOfCrew <= MAX_NUMBER_OF_CREW) && (numberOfCrew >= MIN_NUMBER_OF_CREW)
-                            && (quantityOfProvisions > QUANTITY_OF_PROVISIONS) && (favorableWeather == FAVORABLE_WEATHER)) ||
-                            ((numberOfCrew == MAX_NUMBER_OF_CREW) && (favorableWeather == FAVORABLE_WEATHER) && (quantityOfProvisions > QUANTITY_OF_PROVISIONS))
+                    ((isDamageShip == IS_DAMAGE_SHIP) && (numberOfCrew <= MAX_NUMBER_OF_CREW) && (numberOfCrew >= MIN_NUMBER_OF_CREW) &&
+                            (quantityOfProvisions > QUANTITY_OF_PROVISIONS) && (isFavorableWeather == IS_FAVORABLE_WEATHER)) ||
+                            ((numberOfCrew == MAX_NUMBER_OF_CREW) && (isFavorableWeather == IS_FAVORABLE_WEATHER) && (quantityOfProvisions > QUANTITY_OF_PROVISIONS))
                 }"
     )
 }
