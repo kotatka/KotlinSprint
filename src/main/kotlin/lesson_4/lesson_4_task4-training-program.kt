20b826b499c8765e90c9917704d922dbf6608840
kotatka <tyrrrr5665@gmail.com>
2024-09-30 06:28:28 +0300

    KS-4-4

--- src/main/kotlin/lesson_4/lesson_4_task4-training-program.kt
+++ src/main/kotlin/lesson_4/lesson_4_task4-training-program.kt
@@ -1,17 +1,15 @@
 package lesson_4
 
-const val GROUP_TRAINING_ONE = 1
-const val GROUP_TRAINING_TWO = 0
-
 fun main() {
     val trainingDay = 5
+    val dayParity = trainingDay % 2
 
     println(
         """
-        Упражнения для рук:    ${(trainingDay % 2) == GROUP_TRAINING_ONE}
-        Упражнения для ног:    ${(trainingDay % 2) == GROUP_TRAINING_TWO}
-        Упражнения для спины:  ${(trainingDay % 2) == GROUP_TRAINING_TWO}
-        Упражнения для пресса: ${(trainingDay % 2) == GROUP_TRAINING_ONE}
+        Упражнения для рук:    ${dayParity == 0}
+        Упражнения для ног:    ${dayParity != 0}
+        Упражнения для спины:  ${dayParity != 0}
+        Упражнения для пресса: ${dayParity == 0}
     """.trimMargin()
     )
 }
