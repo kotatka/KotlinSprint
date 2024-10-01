d68fe6f100844c2648df91818b63abb88dfe615c
kotatka <tyrrrr5665@gmail.com>
2024-10-01 11:19:01 +0300

    KS-5-1

--- src/main/kotlin/lesson_5/lesson_5_1_verificaton.kt
+++ src/main/kotlin/lesson_5/lesson_5_1_verificaton.kt
@@ -1,11 +1,11 @@
 package lesson_5
 
-const val CORRECT_ANSWER = 39
-
 fun main() {
-    println("Добрый день, чтобы продолжить авторизацию докажите, что вы не бот. Решите пример '13+26'")
+    val number1 = 13
+    val number2 = 26
+    println("Добрый день, чтобы продолжить авторизацию докажите, что вы не бот. Решите пример '$number1+$number2'")
     val userAnswer = readln().toInt()
 
-    if (userAnswer == CORRECT_ANSWER) println("Добро пожаловать!")
+    if (userAnswer == (number1 + number2)) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 }
