b1eb7d516b1bbd34ab87e565caec1bff16bbadfa
kotatka <tyrrrr5665@gmail.com>
2024-10-05 20:35:16 +0300

    KS-8-5-create-new-recept

---
+++ src/main/kotlin/lesson_8/lesson_8_task5_create_new_recept.kt
@@ -0,0 +1,13 @@
+package lesson_8
+
+fun main() {
+    println("Сколько ингридиентов планироуется в вашем рецепте?")
+    val numsIngridients = readln().toInt()
+    val arrayOfIngridients = Array(size = numsIngridients, { "" })
+    for (i in 0..(numsIngridients - 1)) {
+        println("Введите ингридиент №${i + 1}")
+        arrayOfIngridients[i] = readln()
+    }
+    for (i in arrayOfIngridients)
+        println("Ингридиент ${arrayOfIngridients.indexOf(i) + 1}: $i")
+}
