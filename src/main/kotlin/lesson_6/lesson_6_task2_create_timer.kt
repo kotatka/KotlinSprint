f4d33ed66ef9d9b8b62992a9efb6f0800f1bd45d
kotatka <tyrrrr5665@gmail.com>
2024-10-05 12:14:37 +0300

    KS-6-2-recreate-timer

---
+++ src/main/kotlin/lesson_6/lesson_6_task2_create_timer.kt
@@ -0,0 +1,12 @@
+package lesson_6
+
+fun main() {
+    println("Введите сколько секунд вам необходимо засечь")
+    val userSeconds = readln().toInt()
+    var i = userSeconds
+    while (i > 0) {
+        i--
+        Thread.sleep(1000)
+    }
+    println("прошло $userSeconds секунд")
+}
