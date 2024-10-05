8f0da35ffa1fac1b788b7a61dabfb0a761bebe51
kotatka <tyrrrr5665@gmail.com>
2024-10-05 15:31:30 +0300

    KS-7-2-cheak-sms

---
+++ src/main/kotlin/lesson_7/lesson_7_task2_cheak_sms.kt
@@ -0,0 +1,16 @@
+package lesson_7
+
+fun main() {
+    var numFromSms:Int
+    var userNum:Int
+    do {
+        numFromSms = (1000..9999).random()
+        println("Ваш код авторизации $numFromSms")
+
+        println("Введите код для авторизации")
+        userNum = readln().toInt()
+
+    } while (numFromSms != userNum)
+
+    println("Код введен верно, Добро пожаловать!")
+}
