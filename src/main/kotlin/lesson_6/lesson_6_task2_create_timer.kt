8840a7c59b9f1973973ac952ca81bced304176a7
kotatka <tyrrrr5665@gmail.com>
2024-10-05 17:19:09 +0300

    KS-6-2

--- src/main/kotlin/lesson_6/lesson_6_task2_create_timer.kt
+++ src/main/kotlin/lesson_6/lesson_6_task2_create_timer.kt
@@ -1,15 +1,14 @@
 package lesson_6
 
+const val CONVERT_MILLISECONDS: Long = 1000
+
 fun main() {
     println("Введите сколько секунд вам необходимо засечь")
-    val userSeconds = readln().toInt()
+    val userSeconds: Long = readln().toLong()
 
-    var i = userSeconds
+    val userMilliseconds: Long = userSeconds * CONVERT_MILLISECONDS
 
-    while (i > 0) {
-        i--
-        Thread.sleep(1000)
-    }
+    Thread.sleep(userMilliseconds)
 
     println("прошло $userSeconds секунд")
 }
