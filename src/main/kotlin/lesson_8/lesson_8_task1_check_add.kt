5fdc6f64ac68a9ea7a7c9c9910185f14320f0024
kotatka <tyrrrr5665@gmail.com>
2024-10-07 11:36:22 +0300

    KS-8-1

--- src/main/kotlin/lesson_8/lesson_8_task1_check_add.kt
+++ src/main/kotlin/lesson_8/lesson_8_task1_check_add.kt
@@ -2,9 +2,6 @@ package lesson_8
 
 fun main() {
     val addOnWeek = intArrayOf(4, 5, 8, 3, 12, 15, 18)
-    var addAll = 0
-    for (i in 0..(addOnWeek.size - 1)) {
-        addAll += addOnWeek[i]
-    }
+    val addAll = addOnWeek.sum()
     println(addAll)
 }
