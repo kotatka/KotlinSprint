f92d60628f7b4cb9b00a5b4f6b8c53b867acdeda
kotatka <tyrrrr5665@gmail.com>
2024-10-08 09:16:22 +0300

    KS-9-1-list-ingridients

--- src/main/kotlin/lesson_9/lesson_9_list_ingridients.kt
+++ src/main/kotlin/lesson_9/lesson_9_list_ingridients.kt
@@ -3,7 +3,7 @@ package lesson_9
 fun main() {
     val ingridientsOfRagu = listOf("курица", "лук", "морковь", "чеснок", "перец", "картофель", "соль")
 
-    println("В рецепте есть слудующие элементы:")
+    println("В рецепте есть следующие элементы:")
     ingridientsOfRagu.forEach {
         println(it)
     }
