c895fd3939122d67f77f5ded4f35766408f75ef0
kotatka <tyrrrr5665@gmail.com>
2024-10-09 18:23:24 +0300

    KS-10-2

--- src/main/kotlin/lesson_10/lesson_10_task2_size_reader.kt
+++ src/main/kotlin/lesson_10/lesson_10_task2_size_reader.kt
@@ -5,12 +5,23 @@ const val MIN_LENGHT = 4
 fun main() {
     println("Введите логин")
     val userLogin = readln()
+
     println("Введите пароль")
     val userPassword = readln()
+
     val loginLenght = lenghtReader(userLogin)
     val passwordLenght = lenghtReader(userPassword)
-    if ((loginLenght < MIN_LENGHT) || (passwordLenght < MIN_LENGHT))
+
+    val checkPassword = lengtchCheck(passwordLenght)
+    val checkLogin = lengtchCheck(loginLenght)
+
+    if ((checkLogin == true) || (checkPassword == true))
         println("Логин или пароль недостаточно длинные")
 }
 
 fun lenghtReader(name: String): Int = name.count()
+
+fun lengtchCheck(name: Int): Boolean {
+    val resultCheck = (name < MIN_LENGHT)
+    return resultCheck
+}
