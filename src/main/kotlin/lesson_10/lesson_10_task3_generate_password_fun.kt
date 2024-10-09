b70957765cb9133d5632e44a5c2d14a37763013b
kotatka <tyrrrr5665@gmail.com>
2024-10-09 10:50:59 +0300

    KS-10-3-password-generate-fun

---
+++ src/main/kotlin/lesson_10/lesson_10_task3_generate_password_fun.kt
@@ -0,0 +1,30 @@
+package lesson_10
+
+fun main() {
+    println("Введите длину пароля, которая вам нужна")
+    val lengthPassword = readln().toInt()
+    val userPassword = generatePassword(lengthPassword)
+    println(userPassword)
+}
+
+fun generatePassword(lenght: Int): String {
+    val nums = ('0'..'9')
+    val simbols = arrayOf('!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' ')
+    var password = ""
+    var x: Char
+
+    for (i in 1..lenght) {
+        if (i % 2 == 0) {
+
+            x = simbols[(0..simbols.size - 1).random()]
+            password += x
+
+        } else {
+
+            x = nums.random()
+            password += x
+
+        }
+    }
+    return password
+}
