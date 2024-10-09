c94d5b24164b2048f84d7863f742fbd6979ea703
kotatka <tyrrrr5665@gmail.com>
2024-10-09 18:45:06 +0300

    KS-9-5

--- src/main/kotlin/lesson_9/lesson_9_task5_upgrade_sort_ingridiates.kt
+++ src/main/kotlin/lesson_9/lesson_9_task5_upgrade_sort_ingridiates.kt
@@ -1,6 +1,7 @@
 package lesson_9
 
 fun main() {
+    println("Введите 5 ингридиентов.")
     val userIngridients = MutableList(5) { readln().trim() }.toSet().sorted()
-    println(userIngridients.joinToString().replaceFirstChar { it.uppercase() })
+    println(userIngridients.joinToString(separator = ", ", postfix = ".").replaceFirstChar { it.uppercase() })
 }
