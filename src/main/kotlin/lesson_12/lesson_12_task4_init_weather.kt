d957f0b15d18c901b270eb9ff0f67cd60044381a
kotatka <tyrrrr5665@gmail.com>
2024-10-14 12:10:42 +0300

    KS-12-4

--- src/main/kotlin/lesson_12/lesson_12_task4_init_weather.kt
+++ src/main/kotlin/lesson_12/lesson_12_task4_init_weather.kt
@@ -1,16 +1,16 @@
 package lesson_12
 
-class WeatherDay5(temperatureDay: Int, temperaturesNight: Int, inPrecipitation: Boolean) {
+class WeatherDay5(temperatureDay: Int, temperaturesNight: Int, isPrecipitation: Boolean) {
 
     var temperatureDay = temperatureDay
     var temperatureNight = temperaturesNight
-    var inPrecipitation = inPrecipitation
+    var isPrecipitation = isPrecipitation
 
     fun printWeather() {
         println(
             """Температура днём = $temperatureDay
             |Температура ночью = $temperatureNight
-            |Наличие осадков в течении суток = $inPrecipitation
+            |Наличие осадков в течении суток = $isPrecipitation
         """.trimMargin()
         )
     }
