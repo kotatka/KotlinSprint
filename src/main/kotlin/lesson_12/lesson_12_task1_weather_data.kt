b6f24759898798c1751bbee46901c806dfbfaedf
kotatka <tyrrrr5665@gmail.com>
2024-10-14 12:07:14 +0300

    KS-12-1

--- src/main/kotlin/lesson_12/lesson_12_task1_weather_data.kt
+++ src/main/kotlin/lesson_12/lesson_12_task1_weather_data.kt
@@ -1,14 +1,14 @@
 package lesson_12
 
-class WeatherDay() {
+class WeatherDay {
     var temperatureDay = 0
     var temperatureNight = 0
-    var inPrecipitation = false
+    var isPrecipitation = false
     fun printWeather() {
         println(
             """Температура днём = $temperatureDay
             |Температура ночью = $temperatureNight
-            |Наличие осадков в течении суток $inPrecipitation
+            |Наличие осадков в течении суток $isPrecipitation
         """.trimMargin()
         )
     }
@@ -18,12 +18,12 @@ fun main() {
     val day1 = WeatherDay()
     day1.temperatureDay = 22
     day1.temperatureNight = 12
-    day1.inPrecipitation = false
+    day1.isPrecipitation = false
 
     val day2 = WeatherDay()
     day2.temperatureDay = 16
     day2.temperatureNight = 8
-    day2.inPrecipitation = true
+    day2.isPrecipitation = true
 
     day1.printWeather()
     day2.printWeather()
