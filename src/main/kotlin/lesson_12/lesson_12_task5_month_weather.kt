1c9484c779d2ec1265c5964b2dafeb4e3d7ce587
kotatka <tyrrrr5665@gmail.com>
2024-10-14 12:11:23 +0300

    KS-12-5

--- src/main/kotlin/lesson_12/lesson_12_task5_month_weather.kt
+++ src/main/kotlin/lesson_12/lesson_12_task5_month_weather.kt
@@ -5,14 +5,14 @@ import kotlin.random.Random
 data class WeatherDay6(
     val temperatureDay: Int,
     val temperatureNight: Int,
-    val inPrecipitation: Boolean
+    val isPrecipitation: Boolean
 ) {
 
     fun printWeather() {
         println(
             """Температура днём = $temperatureDay
             |Температура ночью = $temperatureNight
-            |Наличие осадков в течении суток = $inPrecipitation
+            |Наличие осадков в течении суток = $isPrecipitation
         """.trimMargin()
         )
     }
@@ -32,12 +32,12 @@ fun main() {
         var newDay = WeatherDay6(
             temperatureDay = (20..30).random(),
             temperatureNight = (10..20).random(),
-            inPrecipitation = Random.nextBoolean()
+            isPrecipitation = Random.nextBoolean()
         )
         monthWeather.add(newDay)
         allDayTemperatures.add(newDay.temperatureDay)
         allNightTemperatures.add(newDay.temperatureNight)
-        allInPrecipitation.add(newDay.inPrecipitation)
+        allInPrecipitation.add(newDay.isPrecipitation)
     }
 
     val averageDayTemperature = allDayTemperatures.average()
