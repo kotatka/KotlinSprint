620a7cb9ec4197dd1fec9c4a2922ee5f28e185aa
kotatka <tyrrrr5665@gmail.com>
2024-10-17 16:01:54 +0300

    KS-12-3-telephone-directory-list

--- src/main/kotlin/lesson_13/lesson_13_task3_telephone_directory_list.kt
+++ src/main/kotlin/lesson_13/lesson_13_task3_telephone_directory_list.kt
@@ -6,8 +6,8 @@ data class TelephoneDirectoryList(
     val nameCompany: String? = null,
 ) {
     fun printDirectory() {
-        val correctName = nameCompany?.toString() ?: ("<Не указано>")
-        println("Имя:$nameUser, Номер:$telephoneNumber, Компания:$correctName")
+        nameCompany ?: ("<Не указано>")
+        println("Имя:$nameUser, Номер:$telephoneNumber, Компания:$nameCompany")
     }
 }
 
@@ -21,11 +21,9 @@ fun main() {
         TelephoneDirectoryList("Марина", 159753654, "Яндекс"),
         TelephoneDirectoryList("Ярослав", 77777777, "Сбербанк"),
     )
-println(listContacts)
 
-    val allCompanies = listContacts.mapNotNull { it.nameCompany }
-    println(allCompanies)
 
-    val numbersCompanies = listContacts.filter { it.nameCompany!=null }
-    println (numbersCompanies)
+    val numbersCompanies = listContacts.filter { it.nameCompany != null }
+    numbersCompanies.forEach { println(it) }
+
 }
