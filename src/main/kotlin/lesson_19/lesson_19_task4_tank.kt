3f2782661ff471804c545671abf408a1f6abf5ef
kotatka <tyrrrr5665@gmail.com>
2024-10-21 17:39:54 +0300

    KS-19-4-tank

---
+++ src/main/kotlin/lesson_19/lesson_19_task4_tank.kt
@@ -0,0 +1,32 @@
+package lesson_19
+
+enum class Patron(val powerPatron: Int) {
+    BLUE_PATRON(5),
+    GREEN_PATRON(10),
+    RED_PATRON(20),
+    NOTHING(0),
+}
+
+class Tank(val name: String, var typeOfPatron: Patron = Patron.NOTHING) {
+    fun loadPatron(type: Patron) {
+        typeOfPatron = type
+    }
+
+    fun shoot() {
+        println("$name наносит: ${typeOfPatron.powerPatron} урона")
+    }
+}
+
+fun main() {
+    val tank1 = Tank("Катюша")
+    tank1.shoot()
+
+    tank1.loadPatron(Patron.BLUE_PATRON)
+    tank1.shoot()
+
+    tank1.loadPatron(Patron.GREEN_PATRON)
+    tank1.shoot()
+
+    tank1.loadPatron(Patron.RED_PATRON)
+    tank1.shoot()
+}
