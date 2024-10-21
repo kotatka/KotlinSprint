0bc8dcf494e9248ebc41820da163956f62433835
kotatka <tyrrrr5665@gmail.com>
2024-10-21 17:08:29 +0300

    KS-19-2-categories

---
+++ src/main/kotlin/lesson_19/lesson_19_task2_categories.kt
@@ -0,0 +1,48 @@
+package lesson_19
+
+enum class Categories() {
+    CLOATHES {
+        override fun getCategories(): String {
+            return "Cloathes"
+        }
+    },
+    STATIONARY {
+        override fun getCategories(): String {
+            return "Stationary"
+        }
+    },
+    TOYS {
+        override fun getCategories(): String {
+            return "Toys"
+        }
+    },
+    FOOD {
+        override fun getCategories(): String {
+            return "Food"
+        }
+    };
+
+    abstract fun getCategories(): String
+}
+
+class Product(
+    val name: String,
+    val id: Int,
+    val categories: Categories,
+) {
+    fun printProduct() {
+        println("Название товара: $name \nid товара: $id\nКатегория товара: ${categories.getCategories()}\n")
+    }
+}
+
+fun main() {
+    val product1 = Product("Кепка", 1, Categories.CLOATHES)
+    val product2 = Product("Пистолет", 2, Categories.TOYS)
+    val product3 = Product("Апельсин", 3, Categories.FOOD)
+    val product4 = Product("Карандаш", 4, Categories.STATIONARY)
+
+    product1.printProduct()
+    product2.printProduct()
+    product3.printProduct()
+    product4.printProduct()
+}
