4e386f9cf545a73c5badbafac7ad293ef6d8e74b
kotatka <tyrrrr5665@gmail.com>
2024-10-22 17:12:06 +0300

    KS-18-4-package

--- src/main/kotlin/lesson_18/lesson_18_task4_package.kt
+++ src/main/kotlin/lesson_18/lesson_18_task4_package.kt
@@ -1,20 +1,20 @@
 package lesson_18
 
-open class Package(val length:Int){
-   open fun areaCalculated():Int = length*length*6
+open class Package(val length: Int) {
+    open fun areaCalculated(): Int = length * length * 6
 }
 
 class rectangularPackage(
     length: Int,
     val width: Int,
     val height: Int,
-): Package(length){
-    override fun areaCalculated(): Int = (2*length*width)+(2*length*height)+(2*width*height)
+) : Package(length) {
+    override fun areaCalculated(): Int = (2 * length * width) + (2 * length * height) + (2 * width * height)
 }
 
 fun main() {
     val package1 = Package(5)
-    val package2 = rectangularPackage(5,6,6)
+    val package2 = rectangularPackage(5, 6, 6)
 
     val area1 = package1.areaCalculated()
     val area2 = package2.areaCalculated()
