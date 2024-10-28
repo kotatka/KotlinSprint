f53bc1842586d97659152620ecfbb00994d14548
kotatka <tyrrrr5665@gmail.com>
2024-10-28 07:25:35 +0300

    KS-21-2-even-number-sum

--- src/main/kotlin/lesson_21/lesson_21_task2_even_number_sum.kt
+++ src/main/kotlin/lesson_21/lesson_21_task2_even_number_sum.kt
@@ -1,10 +1,6 @@
 package lesson_21
 
-fun List<Int>.evenNumberSum(x:List<Int>):Int{
-var summ = 0
-    x.map { if (x.indexOf(it) % 2 != 0) summ+=it }
-    return summ
-}
+fun List<Int>.evenNumberSum(x: List<Int>): Int = (this.filter { it % 2 == 0 }).sum()
 
 fun main() {
     val list = listOf<Int>(1, 5, 7, 18, 11, 22)
