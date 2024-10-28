4534de840d8ebcef32e5763d0304570056c90f1a
kotatka <tyrrrr5665@gmail.com>
2024-10-28 07:32:04 +0300

    KS-21-5-Map

--- src/main/kotlin/lesson_21/lesson_21_task5_Map.kt
+++ src/main/kotlin/lesson_21/lesson_21_task5_Map.kt
@@ -2,18 +2,13 @@ package lesson_21
 
 fun main() {
     val skills = mapOf("Сила" to 100, "Ловкость" to 99, "Интеллект" to 96, "Выносливость" to 112)
-    val maxCategory = skills.maxCategory(skills)
+    val maxCategory = skills.maxCategory()
     println("Сильнейшая характеристика - $maxCategory")
 }
 
-fun Map<String, Int>.maxCategory(x: Map<String, Int>): String {
-    val sortedMap = x.toList().sortedByDescending { (_, value) -> value }.toMap()
+fun Map<String, Int>.maxCategory(): String {
+    val sortedMap = this.toList().sortedByDescending { (_, value) -> value }.toMap()
     val sortedKeys = sortedMap.keys.toList()
     val maxCategory = sortedKeys[0]
     return maxCategory
 }
-
-
-
-
-
