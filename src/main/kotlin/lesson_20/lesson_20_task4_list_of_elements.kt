bd327bf82dc19361386e1361ee3679fd3f120148
kotatka <tyrrrr5665@gmail.com>
2024-10-28 11:52:27 +0300

    KS-20-4-list-of-elements

--- src/main/kotlin/lesson_20/lesson_20_task4_list_of_elements.kt
+++ src/main/kotlin/lesson_20/lesson_20_task4_list_of_elements.kt
@@ -2,5 +2,7 @@ package lesson_20
 
 fun main() {
     val listOfElements = listOf("Апельсин", "Помидор", "Огурец", "Яйцо", "Мандарин", "Курица", "Соль")
-    listOfElements.map { if (listOfElements.indexOf(it) % 2 != 0) println("Нажат элемент: $it") }
+    val lyambda: (String) -> Unit
+    lyambda = { println("Нажат элемент: $it") }
+    val listOfLyambda = listOfElements.map { if (listOfElements.indexOf(it) % 2 != 0) lyambda(it) }
 }
